c88ebbbe24fccadd0e6b0efe0c8401a2c70325ad
tebasakioisii <tebasaki.kawasaki@gmail.com>
2018-04-24 17:26:32 +0900

    Today, I went to Chapter7 This Chapter descrabes about Routing Address.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,17 @@ class UsersController < ApplicationController
   end
 
   def show
+	  @user = Hash.new
+	  if params[:username] == 'tebasaki'
+		  @user[:name] = 'Tebasaki Tarou'
+		  @user[:username] = 'tebasaki'
+		  @user[:location] = 'Tokyo, JP'
+		  @user[:about] = 'Nice to meet you.'
+	  elsif params[:username] == 'luling'
+		  @user[:name] = 'No Name'
+		  @user[:username] = 'luling'
+		  @user[:location] = 'Niigata, JP'
+		  @user[:about] = '○oo○s!!!'
+	  end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'users/index'
-  get 'users/show'
+  get 'users/show/:username' => "users#show"
   get 'user/index'
   get 'user/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
