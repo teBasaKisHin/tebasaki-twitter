56d706046aa670a510826c59d543e580d8523a90
tebasakioisii <tebasaki.kawasaki@gmail.com>
2018-04-28 12:56:38 +0900

    goto ch12

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+@user = User.new
+@user.name = 'Tbasaki Tarou'
+@user.username = 'tebasaki'
+@user.location = 'Tsu, JP'
+@user.about = 'Hello, I\'m Japanese'
+@user.save
+
+@user = User.new
+@user.name = 'King Luliluli'
+@user.username = 'luling'
+@user.location = 'Naha, JP'
+@user.about = 'I am King Of Luliluli Empire'
+@user.save
