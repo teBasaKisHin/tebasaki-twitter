ad064f8ea4bee9c6b3822ddd30ab29835e38882e
tebasakioisii <tebasaki.kawasaki@gmail.com>
2018-04-28 22:00:41 +0900

    Went to Ch13.Today I finished on This Chapter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,6 @@ class UsersController < ApplicationController
   end
 
   def show
-	  @user = Hash.new
-	  if params[:username] == 'tebasaki'
-		  @user[:name] = 'Tebasaki Tarou'
-		  @user[:username] = 'tebasaki'
-		  @user[:location] = 'Tokyo, JP'
-		  @user[:about] = 'Nice to meet you.'
-	  elsif params[:username] == 'luling'
-		  @user[:name] = 'No Name'
-		  @user[:username] = 'luling'
-		  @user[:location] = 'Niigata, JP'
-		  @user[:about] = '○oo○s!!!'
-	  end
+	  @user = User.find_by(:username => params[:username])
   end
 end
